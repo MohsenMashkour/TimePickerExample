88e686456cd5e3c9abbe1d1749163c21edf5b8c8
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-05-30 13:46:45 +0200

    first init

---
+++ app/src/main/java/com/mkrdeveloper/timepickerexample/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mkrdeveloper.timepickerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.TimePicker
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tp = findViewById<TimePicker>(R.id.timePicker)
+        val txt = findViewById<TextView>(R.id.textView)
+
+        tp.setOnTimeChangedListener { view, hourOfDay, minute ->
+
+            var hour = hourOfDay
+            var format = ""
+
+            if (hour >= 12){
+                format = "PM"
+                if (hour != 12)
+                    hour -= 12
+            }else{
+                format = "AM"
+            }
+
+            txt.text = addZero(hour) + " : " + addZero( minute) + " $format"
+        }
+    }
+
+    fun addZero (num: Int): String{
+        return if (num <= 9) "0$num" else num.toString()
+    }
+}
